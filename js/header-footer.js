22457089b0725f65f6f274602f02823319ca123b
MingXie15 <32473417+CarterXie@users.noreply.github.com>
2019-01-07 22:59:40 -0800

    add

---
+++ js/header-footer.js
@@ -0,0 +1,24 @@
+$(document).scroll(function() {
+	if ($(document).scrollTop() > 0) {
+		$('.top-nav').addClass('top-nav-aniamte');
+	} else {
+		$('.top-nav').removeClass('top-nav-aniamte');
+	}
+})
+console.log($(window).width())
+$(document).ready(function() {
+	$('.configHeader').load('header.html', function() {
+		var active = $(this).attr('data-active')
+		if (active) {
+			$(this).find('.' + active).addClass('active').siblings().removeClass('active');
+		}
+	});
+	$('.configFoot').load('footer.html');
+	$('.configHeader').on('click', '.dropdown-toggle', function() {
+		if ($('.dropdown-menu').is(':visible')) {
+			$('.dropdown-menu').slideUp(150)
+		} else {
+			$('.dropdown-menu').slideDown(150)
+		}
+	})
+})
